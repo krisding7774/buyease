3c07e8b35fa395c1054e780fbcfc5d5ac00c0a0a
ASGARD\ricky_wang <Ricky_Wang@asgard.com.tw>
2023-11-08 16:20:53 +0800

    project init

---
+++ src/test/java/com/phw/initial/BuyeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phw.initial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
